3e2df786e471b1664bc54a0d1b0a7055ffccc5b7
ohkinozomu <nozomunoise@gmail.com>
2024-06-01 07:02:53 +0900

    Add index.d.ts

---
+++ ts/index.d.ts
@@ -0,0 +1,11 @@
+export function runSqldef(
+    dbType: string,
+    dbName: string,
+    user: string,
+    password: string,
+    host: string,
+    port: string,
+    schemaFile: string,
+    enableDropTable: number
+): number;
+  
